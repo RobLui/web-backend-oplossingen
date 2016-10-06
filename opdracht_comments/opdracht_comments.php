aebf462eaeea0cc03d1dbf7649e97e49d878a05b
Robbert Luit <robbertluit@hotmail.com>
2016-10-06 15:58:18 +0200

    opdracht_comments_toevoegen

---
+++ opdracht_comments/opdracht_comments.php
@@ -0,0 +1,24 @@
+<?php 
+
+echo "Robbert Luit";
+
+?>
+
+
+
+
+    <!doctype html>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body class="web-backend-opdracht">
+
+        <section class="body">
+
+        </section>
+    </body>
+
+    </html>
