ad0cacff0dddace953245b5e512ce2575c94cc40
Robbert Luit <robbertluit@hotmail.com>
2016-10-10 22:02:05 +0200

    commentaar toegevoegd -for-

--- opdracht_looping_statements_while/opdracht_looping_statements_while_deel_1.php
+++ opdracht_looping_statements_while/opdracht_looping_statements_while_deel_1.php
@@ -1,36 +1,32 @@
-<?php 
+<!doctype html>
+<html>
 
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Opdracht while</title>
+    <link rel="stylesheet" href="http://web-backend.local/css/global.css">
+    <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
+    <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
+</head>
 
-    
-?>
+<body class="web-backend-opdracht">
 
+    <section class="body">
 
-    <!doctype html>
-    <html>
+        <h1>Opdracht while: deel 1</h1>
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Opdracht while</title>
-        <link rel="stylesheet" href="http://web-backend.local/css/global.css">
-        <link rel="stylesheet" href="http://web-backend.local/css/facade.css">
-        <link rel="stylesheet" href="http://web-backend.local/css/directory.css">
-    </head>
+        <ul>
 
-    <body class="web-backend-opdracht">
+            <li>Druk alle getallen af van 0 tot 100 afgescheiden door een komma en een spatie ' , '.</li>
+            <li>Op een volgende lijn druk je alle getallen af die deelbaar zijn door 3 én groter zijn dan 40 mààr kleiner zijn dan 80.</li>
 
-        <section class="body">
+            <p>
 
-            <h1>Opdracht while: deel 1</h1>
+                <?php  
 
-            <ul>
-
-                <li>Druk alle getallen af van 0 tot 100 afgescheiden door een komma en een spatie ' , '.</li>
-                <li>Op een volgende lijn druk je alle getallen af die deelbaar zijn door 3 én groter zijn dan 40 mààr kleiner zijn dan 80.</li>
-
-                <p>
-
-                    <?php        
+            //for lus gebruikt voor deze oefening op te lossen, navragen bij Pascal of ik een while "moest" gebruiken
+            //anders moet ik deze oefening nog aanpassen.
                 for($i = 0; $i<=100; $i++)
                 {
                     if ($i != 100)
@@ -52,15 +48,15 @@
                        }
                 }
                 ?>
-                </p>
+            </p>
 
 
 
 
-            </ul>
+        </ul>
 
 
 
-    </body>
+</body>
 
-    </html>
+</html>
