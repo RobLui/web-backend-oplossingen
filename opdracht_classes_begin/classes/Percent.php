83255380c4803c3cb1238738296e4f2c8c185ba2
Robbert Luit <robbertluit@hotmail.com>
2016-11-24 13:43:24 +0100

    applic & percent

---
+++ opdracht_classes_begin/classes/Percent.php
@@ -0,0 +1,45 @@
+<?php
+
+class Percent
+{
+  public $absolute;
+  public $relative;
+  public $hundred;
+  public $nominal;
+  public function _construct($new,$unit)
+  {
+  //$this gebruiken omdat de variabelen in de klasse zelf zich bevinden
+    $this -> $absolute = $new/$unit;
+    $this -> $relative = $absolute -1;
+    $this -> $hundred = $absolute * 100;
+
+    //groter dan 1
+    if ($this -> $absolute > 1) {
+      $this -> $nominal = 'positive';
+    }
+    //gelijk aan 1
+    elseif ($this -> $absolute == 1) {
+      $this -> $nominal = 'status-quo';
+    }
+    //kleiner dan 1
+    elseif ($this -> $absolute < 1) {
+      $this -> $nominal = 'negative';
+    }
+  }
+  // string number_format ( float $number [, int $decimals = 0 ] ) == afronden met 2 decimalen
+  public function formatNumber($number)
+  {
+    return number_format(number,2);
+  }
+}
+
+//TEST
+// function callFormatFunction()
+// {
+//   class $Percentclass = new Percent();
+//   $Percentclass->formatNumber(2);
+// }
+// var $a = callFormatFunction();
+// print_r(a);
+
+ ?>
