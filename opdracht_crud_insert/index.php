9b57743b33a92290f4e9554b90e52015e91286b5
Robbert Luit <robbertluit@hotmail.com>
2016-12-04 17:32:04 +0100

    crud_insert

---
+++ opdracht_crud_insert/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="index.html" method="post">
+
+    <ul>
+  <li>
+    <label for="brnaam">Brouwernaam</label>
+    <input type="text" name="brnaam" id="brnaam">
+  </li>
+  <li>
+    <label for="adres">Adres</label>
+    <input type="text" name="adres" id="adres">
+  </li>
+  <li>
+    <label for="postcode">Postcode</label>
+    <input type="text" name="postcode" id="postcode">
+  </li>
+  <li>
+    <label for="gemeente">Gemeente</label>
+    <input type="text" name="gemeente" id="gemeente">
+  </li>
+  <li>
+    <label for="omzet">Omzet</label>
+    <input type="text" name="omzet" id="omzet">
+  </li>
+</ul>
+</form>
+
+  </body>
+</html>
