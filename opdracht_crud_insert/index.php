bd0a991d926c37fbf21515a0e541d6abfe17c9a7
Robbert Luit <robbertluit@hotmail.com>
2016-12-05 17:19:38 +0100

    werkende versie insert :)

--- opdracht_crud_insert/index.php
+++ opdracht_crud_insert/index.php
@@ -7,30 +7,50 @@
 //     ':first_name' => '~John',
 //     ':last_name' => '~Doe'
 // ));
+if (isset($_POST["submit"])) {
 try {
   //verbinding database
   $db = new  PDO('mysql:host=localhost;dbname=bieren', 'root','');
   //query && zet de waardes op verschillende variabelen
-  $db_query = "INSERT INTO 'brouwers'
-  ('brnaam', 'adres', 'postcode', 'gemeente', 'omzet')
+  $db_query = "INSERT INTO brouwers
+  (brnaam, adres, postcode, gemeente, omzet)
   VALUES(:brnaam,:adres,:postcode,:gemeente,:omzet)";
   //query in db
   $db_access = $db->prepare($db_query);
-  //voer query in db uit , maakt gebruik van eerder gezette variabelen (in de db_query)
-  $db_access->execute(array(
-        ':brnaam' => 'testuser',
-        ':adres' => 'test adres',
-        ':postcode' => 'test postcode',
-        ':gemeente' => 'test gemeente',
-        ':omzet' => '5'
-  ));
-  // var_dump($db_access);
-}
+  //waarde van de laatste ingeserte id?
 
+  //voer query in db uit , maakt gebruik van eerder gezette variabelen (in de db_query)
+  //
+  // $db_access->bindValue( ':brnaam', $_POST[ 'brnaam' ] );
+  // $db_access->bindValue( ':adres', $_POST[ 'adres' ] );
+  // $db_access->bindValue( ':postcode', $_POST[ 'postcode' ] );
+  // $db_access->bindValue( ':gemeente', $_POST[ 'gemeente' ] );
+  // $db_access->bindValue( ':omzet', $_POST[ 'omzet' ] );
+  // $db_access->execute();
+
+  // Methode die teruggeeft wat de id was van de laatste insert in de database, standaard beschikbare methode
+
+  //voer query in db uit , maakt gebruik van eerder gezette variabelen (in de db_query)
+  // --- WERKT -- QUERY zat voordien de fout in waardoor het niet werkte...
+  $db_access->execute(array(
+        ':brnaam'   => $_POST[ 'brnaam' ],
+        ':adres'    => $_POST[ 'adres' ],
+        ':postcode' => $_POST[ 'postcode' ],
+        ':gemeente' => $_POST[ 'gemeente' ],
+        ':omzet'    => $_POST[ 'omzet' ],
+  ));
+
+  $insertId =	$db->lastInsertId();
+  var_dump($insertId);
+}
 catch (PDOException $e) {
   //foutmelding bij geen verbinding
 echo "Hier liep het fout: " . $e->getMessage();
 }
+  // var_dump($db_access);
+}
+
+
 
 
 
