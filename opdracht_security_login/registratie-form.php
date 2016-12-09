ced81aa0d80f437fabc9f31375aef8c817710e0d
Robbert Luit <robbertluit@hotmail.com>
2016-12-09 23:28:10 +0100

    sessie working toegevoegd, random pass and swithed code from one to another php file, misplaced them first

--- opdracht_security_login/registratie-form.php
+++ opdracht_security_login/registratie-form.php
@@ -1,26 +1,11 @@
 <?php
+session_start();
 
-function generatePassword(){
-  $letters_klein = "abcdefghijklmnopqrstuvwxyz";
-  $letters_groot = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  $min_number = 1;
-  $max_number = 100;
-  $password_klein = substr( str_shuffle( $letters_klein ), 0, 8 );
-  $password_groot = substr( str_shuffle( $letters_groot ), 0, 8 );
-  $password_numbers = mt_rand($min_number,$max_number);
-  //kleine letters
-  var_dump($password_klein);
-  //grote letters
-  var_dump($password_groot);
-  //mt_rand returnt random number tss min en max
-  var_dump($password_numbers);
-  //combinatie vn de 3..
-  return ($password_klein . $password_numbers . $password_groot);
+if (isset($_SESSION['session_pass'])) {
+  var_dump($_SESSION['session_pass']);
+}
+else {
+echo "niet gelukt";
 }
 
-
-if (isset($_POST["generate_pass"])) {
-  echo "test";
-  var_dump(generatePassword());
-}
 ?>
