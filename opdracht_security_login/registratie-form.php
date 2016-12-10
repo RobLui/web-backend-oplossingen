a3eb8dfea424d8836d1bab304fb63917bbc55743
Robbert Luit <robbertluit@hotmail.com>
2016-12-10 22:38:55 +0100

    further changes made working :)

--- opdracht_security_login/registratie-form.php
+++ opdracht_security_login/registratie-form.php
@@ -5,7 +5,8 @@ session_start();
 
 //als de session_pass is gezet op vorige pass mag die in deze pagina gebruikt worden
 if (isset($_SESSION['session_pass'])) {
-
+$email_db = "";
+$pass_db = $_SESSION['session_pass'];
 // var_dump($_SESSION['session_pass']); -> Check welk passwoord is aangemaakt -> shh! :)
 }
 else {
--- opdracht_security_login/registratie-process.php
+++ opdracht_security_login/registratie-process.php
@@ -18,13 +18,39 @@ function generatePassword(){
   return ($password_klein . $password_numbers . $password_groot);
 }
 
-
 if (isset($_POST["generate_pass"])) {
   session_start();
   $_SESSION["session_pass"] = generatePassword();
-  echo "test";
+  // echo "test";
   header("location:registratie-form.php");
   // var_dump(generatePassword());
+}
+
+if (isset($_POST["email"])) {
+  // ---------- CONTROLEER OP GELDIGHEID VAN EMAIL ----------
+  //start sessie
+  session_start();
+  //zet sessie gelijk aan de post value die in email zat
+  $SESSION["email"] = $_POST["email"];
+  //lokale var die we makkelijk knnen gebruiken hier
+  $email = $_POST["email"];
+  // var_dump($email);
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    //errorboodschap
+    $emailErr = "Invalid email format";
+    echo $emailErr;
+    //stuur terug naar de beginpage
+    header("location: http://oplossingen.web-backend.local/opdracht_security_login/form.html");
+  }
+  else {
+    //alles juist
+    echo "welcome!";
+  }
+}
+else{
+  // mocht er toch nog iets anders mis lopen..
+  echo "vul een juist emailadres in plox";
+  header("location: http://oplossingen.web-backend.local/opdracht_security_login/form.html");
 }
 
 // ---------- CONTROLEER OP GELDIGHEID VAN NAAM ---------- VB W3SCHOOLS
@@ -33,11 +59,23 @@ if (isset($_POST["generate_pass"])) {
 //   $nameErr = "Only letters and white space allowed";
 // }
 
-// ---------- CONTROLEER OP GELDIGHEID VAN EMAIL ---------- VB W3SCHOOLS
-// $email = test_input($_POST["email"]);
-// if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-//   $emailErr = "Invalid email format";
-// }
+
+try {
+  //connectie met db
+  $db = new PDO('mysql:host=localhost;dbname=opdracht-security-login', 'root','');
+  //query && zet de waardes op verschillende variabelen
+  $db_query = "SELECT * FROM users";
+  //query in db
+  $db_access = $db->prepare($db_query);
+  //voer uit
+  $db_access->execute();
+  // var_dump($db_access);
+}
+catch (PDOException $e) {
+//foutbericht
+echo "Hier liep het fout " . $e->getMessage();
+}
+
 
 
 ?>
