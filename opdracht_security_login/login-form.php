4ab908d9f97c05854ca9dda14d720f0a8ee3dbfb
Robbert Luit <robbertluit@hotmail.com>
2016-12-14 01:08:19 +0100

    test cookies added

--- opdracht_security_login/login-form.php
+++ opdracht_security_login/login-form.php
@@ -1,15 +1,30 @@
 <?php
 session_start();
 
+$login_state;
+
 if(isset($_SESSION))
 {
-  $msg = $_SESSION["email"] . " = email en ";
-  $pwd =  " paswoord = " . $_SESSION["session_pass"];
-  echo $msg . " " . $pwd;
+  $mail = $_SESSION["email"];
+  $pwd = $_SESSION["session_pass"];
+  echo $mail . " = message <br> " . $pwd . " = paswoord " ;
+  $login_state = true;
+  // test cookies
+  setcookie("mail_cookie", $mail);
+  setcookie('pwd_cookie', $pwd);
 }
 else {
-  var_dump("Dit zijn alle sessions die aangemaakt zijn: " . $_SESSION);
+  // var_dump("Dit zijn alle sessions die aangemaakt zijn: " . $_SESSION);
 }
-// var_dump($_SESSION);
 
+if (isset($mail))
+{
+// var_dump($mail);
+}
+else {
+  header("location: http://oplossingen.web-backend.local/opdracht_security_login/registratie-form.php");
+}
+// kill sessie
+session_destroy();
+var_dump($_COOKIE);
  ?>
