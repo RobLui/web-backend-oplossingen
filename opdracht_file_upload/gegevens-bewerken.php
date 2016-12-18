e6c9a4eb1a3701e7c050607b1d4b701d88611ca2
Robbert Luit <robbertluit@hotmail.com>
2016-12-18 16:11:56 +0100

    uploading finished , works with all checks :)!

--- opdracht_file_upload/gegevens-bewerken.php
+++ opdracht_file_upload/gegevens-bewerken.php
@@ -6,32 +6,39 @@ $can_upload = 0;   // mag standaard niet uploaden, tenzij ergens gelijk aan 1 (t
 // Check if image file is a actual image or fake image
 
 if (isset($_POST["submit"])) {  //btn submit pressed
-  if (isset($_FILES["fileToUpload"] )) //als er een file is
+/* 1 */
+    if (isset($_FILES["fileToUpload"] )) //als er een file is
     {
       $target_file = $_FILES["fileToUpload"]["name"];
-      if (empty($target_file)) {
-        $_SESSION["boodschap"] = "No file found, pls enter a file to upload";
-        header("location: gegevens-wijzigen-form.php");
+      /* 2 */
+      if (empty($target_file))
+      {
+      $_SESSION["boodschap"] = "No file found, pls enter a file to upload";
+      header("location: gegevens-wijzigen-form.php");
       }
+      /* 2 */
       else
       {
-      $_SESSION["show_target_file"] = $target_file;
+      // $_SESSION["show_target_file"] = $target_file;
       $dir_location = "img/";   //dir waar de file naartoe moet geupload worden - deze oefening img/
       $file_location = $dir_location . basename($target_file); //welk pad de file moet in terechtkomen bv: oplossingen/opdracht_file_upload/..
       $image_extension = pathinfo($file_location,PATHINFO_EXTENSION); //geeft de extensie van het bestand terug, gebruikt voor te checken jpeg, jpg, etc
 
       $check_upload = getimagesize($_FILES["fileToUpload"]["tmp_name"]); //check of het een image is
       $file_size = $_FILES["fileToUpload"]["size"];
-
-      if ($file_size > 2048) { // 2 mb = 2048 kb
-        $_SESSION["boodschap"] = "The file is too large";
+      /* 2 */
+      if ($file_size > 2048) // 2 mb = 2048 kb
+      {
+        $_SESSION["boodschap"] = "The file is too large"; // Het bestand mag niet groter zijn dan 2mb
         $can_upload = 0;
       }
-      if($check_upload) // true, is een image
+      /* 2 */
+      if ($check_upload) // true, is een image
       {
           $_SESSION["boodschap"] =  "File is an image - " . $check_upload["mime"]; //mime geeft de soort extentie terug in woorden (image/jpeg) in dit geval bij bv jpg extentie
-          $can_upload = 1; //mag uploaden
+          $can_upload = 1; //mag uploaden // want het moet een .png, .jpg of .gif zijn
       }
+      /* 2 */
       else // false, is een andere soort extentie - laat zien de welke
       {
           $_SESSION["boodschap"] = "Wrong file extention - " . $image_extension; // geeft weer welke file extention het dan wel is als het niet jpeg/img is
@@ -42,19 +49,12 @@ if (isset($_POST["submit"])) {  //btn submit pressed
 
       // $_SESSION["boodschap"] = "can upload = " . $can_upload;
 
-  if (file_exists($file_location))
-    {
-      $_SESSION["boodschap"] = "file bestaat al";
-      $can_upload = 0; //can't upload
-    }
-
-  // Het moet een .png, .jpg of .gif zijn // Het bestand mag niet groter zijn dan 2mb // Als er een error is op het $_FILES-niveau
-  if ($error_files_lvl)
-    {
-  // moet er geredirect worden naar gegevens-wijzigen-form.php en de gepaste boodschap getoond worden.
-    header("location: gegevens-wijzigen-form.php");
-    $_SESSION["boodschap"] = "Error op files niveau";
-    }
+      /* 2 */
+      if (file_exists($file_location))
+      {
+        $_SESSION["boodschap"] = "file bestaat al";
+        $can_upload = 0; //can't upload
+      }
 
   if ($can_upload) //als je mag uploaden
     {
@@ -78,15 +78,30 @@ if (isset($_POST["submit"])) {  //btn submit pressed
         $_SESSION["boodschap"] = "uploaded";
     }
   }
+
+  /* 2 */
+  if (is_dir($dir_location) && $can_upload) //check for the directory waar je moet in uploaden bestaat // $_FILES-niveau geen errors geeft 1 als het bestaat
+    {
+      /* 3 */
+      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $file_location))
+      {
+       // $_SESSION["show_target_file"] = is_dir($dir_location);
+       $_SESSION["boodschap"] = "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+      }
 }
 
-  else{
-    echo "upload een file plox";
-    header("location: gegevens-bewerken.php");
-  }
+/* 1 */
+else
+{
+  echo "upload een file plox";
+  header("location: gegevens-wijzigen-form.php");
+}
+
   // header("location: gegevens-wijzigen-form.php");
   // Als het bestand voldoet aan de voorwaarden moet de bestandsnaam als volgt worden samengesteld: timestamp_bestandsnaam.extensie
 }
+  }
+
 ?>
 
 <!DOCTYPE html>
