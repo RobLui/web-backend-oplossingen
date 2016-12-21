0a2a7c86b763449b55c5ded429b8ead9ba570610
Robbert Luit <robbertluit@hotmail.com>
2016-12-21 12:30:41 +0100

    ajax QUESTION for Pascal , if he can explain

--- opdracht_ajax/contact_form.php
+++ opdracht_ajax/contact_form.php
@@ -30,6 +30,8 @@ if(isset($_SESSION["foutboodschap"])){
     </style>
   </head>
   <body>
+
+  <div id="form_wrapper">
     <form class="email_form" action="contact.php" method="post" id="email_form">
       <label for="email" >E-mail:</label>
       <input type="email" name="email" class="style-email" value="<?php if (isset($_SESSION["email"])){echo $_SESSION["email"];} ?>">
@@ -38,18 +40,36 @@ if(isset($_SESSION["foutboodschap"])){
       <input type="checkbox" name="checkbox" >Stuur een kopie naar mezelf<br>
       <button type="submit" name="verzenden" class="style-submit">verzenden</button>
     </form>
-    <div id="test_echo">
-    </div>
+  </div>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script> <!-- jQuery library toevoegen -->
     <script type="text/javascript">
-      $(document).ready(function(){ // Handler for .ready() called
-        // let form_input= $( "#email_form :input" ); // input
-        // let form_submit= $( "#email_form :submit" ); // submit
-        let form_data = $("#email_form").submit(function(){ // alles
-      } //hoort bij de .submit
-    ) //hoort bij de .submit
-  } //hoort bij de .ready
-); //hoort bij de .ready
+  	$(function(){
+    $('#email_form').submit(function(){
+  					$.ajax({
+  						type: 'POST',
+  						url: 'contact_api.php',
+  						data: $('#email_data').serialize()
+  						success: function(data) {
+  							data = JSON.parse(data);
+  										if (data['type'] == "success")
+  										{
+  											$('$#form_wrapper').fadeOut('slow', function(){
+
+  												$('#form_wrapper').append('<p>Bedankt! Uw bericht is goed verzonden!</p>').hide().fadeIn('slow');
+  											});
+
+  										}
+  										else if (data['type'] == "error")
+  										{
+  											$('#form_wrapper').prepend('<p>Oeps, er ging iets mis. Probeer opnieuw!</p>').hide().fadeIn('slow');
+  											// Wat gebeurt er in deze opstelling als er opnieuw iets mis gaat? Hoe kan je dit verhelpen?
+  										}
+  									}
+  						})
+  					return false;
+          })
+        })
     </script>
   </body>
 </html>
